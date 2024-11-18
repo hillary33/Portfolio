76919b239b2d091bbd9da95cf056484ae6ec5932
Hillary <hillary.ferreir@gmail.com>
2024-11-17 23:09:55 -0300

    feat: primeiras implantações

---
+++ portfolio_hillary/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import './components/Navbar';
+import Navbar from './components/Navbar';
+
+function App() {
+  return(
+    <> 
+      <Navbar/>
+      <p>Olá, sou a Hillary! Recém formada em Análise e desenvolvimento de Sistemas e apaixonada por desenvolvimento web, e cheia de vontade para aprender</p>
+      <p>Venha conhecer mais sobre mim</p>
+    </>
+  )
+}
+
+export default App;
---
+++ portfolio_hillary/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+        <>
+            <ul className="navbar">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#about">SOBRE</a></li>
+                <li><a href="#project">PROJETOS</a></li>
+                <li><a href="#certificate">CERTIFICADOS</a></li>
+                <li><a href="contact">CONTATO</a></li>            
+            </ul>  
+        </>
+    )
+}
+
+export default Navbar;
